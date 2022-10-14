aa30c4f28d006c06b74a733e58090a606981e0db
ennaesor <sherylvelasco340@gmail.com>
2022-10-14 01:02:51 -0300

    javascript_exercise_2

--- htmlcss/js kodego.js
+++ htmlcss/js kodego.js
@@ -3,3 +3,51 @@ function getAreaOfCircle(radius){
   var area = (pi * radius) * (pi * radius)
   return area;
 }
+
+function doCheckOddorEven(value){
+  if(value % 2 == 0){
+    return "ODD";
+  }else{
+    return "EVEN";
+  }
+}
+
+function doCheckVowelorConsonant(char){
+  char = char.toLowerCase();
+  if(char == "a" || char == "e" || char == "i" || char == "o" || char == "u" ){
+    return "vowel";
+  }else{
+    return "consonant";
+  }
+}
+
+function getFlowerColor(flower){       
+  switch(flower){
+     case "rose":
+        return "red";        
+     case "violet":
+        return "blue";      
+     case "sunflower":
+        return "yellow";   
+     default:
+        return "unknown";
+   }   
+ }
+
+ function getCalendarMonthDays(monthName){
+  monthName = monthName.toLowerCase();
+  switch(monthName){
+    case "september":
+    case "april":
+    case "june":  
+    case "november": 
+       return 30;
+    case "february":
+       return 28;
+    default:
+       return 31;
+   
+  }
+
+ }
+ 
