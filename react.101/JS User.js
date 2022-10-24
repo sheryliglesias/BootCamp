e550545dff6e3425f6a0cf09de70e8e311eb2c36
ennaesor <sherylvelasco340@gmail.com>
2022-10-23 22:55:29 -0300

    add

--- react.101/JS User.js
+++ react.101/JS User.js
@@ -1,14 +1,41 @@
+<!DOCTYPE html>
+<html>
+
+<body>
+      
+       
+
+<script>
+
 class User{
-  constructor(email, password, firstName, lastName){
+  constructor(email, password){
       this.email = email;
       this.password = password;
-      this.firstName = firstName;
-      this.lastName = lastName;
+      
+  }
+  getPassword(){
+    return "Your default password is" + this.password;
   }
 }
 
-let user1 = new User("juan@kodego,ph","12345678","Juan", "dela Cruz");
-let user2 = new User("maria@kodego,ph","12345678","Maria", "Makiling");
+class PersonalInfo extends User{
+  constructor(email, password, firstName, lastName){
+     this.firstName = firstName;
+     this.lastName = lastName;
+     super(email, password); 
+   
+    }
+  } 
 
-user2.firstName;
+  fullName(){
+    return this.firstName + " " + this.lastName;
+  }
+  
+let user1 = new PersonalInfo("juan@kodego,ph","12345678","Juan", "dela Cruz");
+let user2 = new PersonalInfo("maria@kodego,ph","12345678","Maria", "Makiling");
 
+document.write(user1.fullName());
+
+</script>
+</body>
+</html>
